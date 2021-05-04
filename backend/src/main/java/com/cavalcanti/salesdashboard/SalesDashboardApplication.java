89164a8e1ddae827c37ab9457d0fe946a128df16
Igor Cavalcanti <igorwc@gmail.com>
2021-05-03 22:30:18 -0300

    Project created

---
+++ backend/src/main/java/com/cavalcanti/salesdashboard/SalesDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.cavalcanti.salesdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cavalcanti/salesdashboard/SalesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavalcanti.salesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
